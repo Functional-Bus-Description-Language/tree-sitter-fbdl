ca9a1dc67cc2a419453d97cd25a4ab7b883c40a1
Michał Kruszewski <mkru@protonmail.com>
2021-07-20 13:41:40 +0200

    Add element definition parsing.

--- grammar.js
+++ grammar.js
@@ -13,7 +13,9 @@ module.exports = grammar({
   ],
 
   rules: {
-    description: $ => repeat(choice($._meta_statement)),
+    description: $ => repeat(choice($._meta_statement, $.element_definition)),
+
+    comment: $ => token(seq('#', /.*/)),
 
     identifier: $ => /([A-Z]|[a-z])([A-Z]|[a-z]|[0-9]|_)*/,
 
@@ -36,6 +38,36 @@ module.exports = grammar({
       $._dedent
     ),
 
-    comment: $ => token(seq('#', /.*/)),
+    parameter: $ => seq($.identifier, optional(seq('=', $.identifier))),
+
+    // TODO: Replace identifier on RHS.
+    parameters_list: $ => seq(
+      '(', $.parameter, repeat(seq(',', $.parameter)), ')'
+    ),
+
+    element_type: $ => choice(
+      'mask',
+    ),
+
+    element_definition: $ => seq(
+      $.element_type, $.identifier,
+      choice(
+        $._newline,
+        optional(seq(optional($.parameters_list), $.element_body))
+      )
+    ),
+
+    element_body: $ => seq(
+      $._newline,
+      $._indent,
+      repeat1(choice(
+        $.property_assignment
+      )),
+      $._dedent
+    ),
+
+    // TODO: Replace identifier on RHS.
+    property_assignment: $ => seq($.identifier, '=', $.identifier),
+
   }
 });
